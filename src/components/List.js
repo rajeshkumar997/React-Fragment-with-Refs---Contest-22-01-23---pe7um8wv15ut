cb9560ba7794c303662bc00f3478cc8c4e57ed97
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-22 20:05:52 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -4,11 +4,9 @@ import ListItems from './ListItems';
  const List = ({listx}) =>{
   return(
     <>
-     <ul>
-         <li id='list' className='items'>
-             {listx.map((list) => (<ListItems valuex = {list} />))}
-         </li>  
-    </ul>
+   {listx.map((i) => {
+   return <ListItems valuex={i} />;
+  })}
    </>
   )
 }
